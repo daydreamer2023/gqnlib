24b28d56abcd0935559848f51753cbe0ada2e22f
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-26 21:03:06 +0900

    Update test case for dataset

--- tests/test_scene_dataset.py
+++ tests/test_scene_dataset.py
@@ -1,23 +1,36 @@
 
 import unittest
 
+import gzip
 import pathlib
+import tempfile
+
+import torch
 
 import gqnlib
 
 
 class TestSceneDataset(unittest.TestCase):
 
-    def setUp(self):
-        root = pathlib.Path(__file__).parent.parent
-        root /= "data/shepard_metzler_5_parts_torch/train/"
-        self.dataset = gqnlib.SceneDataset(root)
-
     def test_len(self):
+        self.dataset = gqnlib.SceneDataset(".")
         self.assertGreaterEqual(len(self.dataset), 0)
 
     def test_getitem(self):
-        frames, cameras = self.dataset[1]
+        # Dummy data
+        imgs = torch.empty(15, 64, 64, 3)
+        tgts = torch.empty(15, 5)
+        data = [(imgs.numpy(), tgts.numpy())] * 20
+
+        with tempfile.TemporaryDirectory() as root:
+            path = str(pathlib.Path(root, "1.pt.gz"))
+            with gzip.open(path, "wb") as f:
+                torch.save(data, f)
+
+            # Access data
+            self.dataset = gqnlib.SceneDataset(root)
+            frames, cameras = self.dataset[1]
+
         self.assertTupleEqual(frames.size(), (20, 15, 3, 64, 64))
         self.assertTupleEqual(cameras.size(), (20, 15, 7))
 
