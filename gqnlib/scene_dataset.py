bb40ebd3fc221f397f7fa59133df0726609d53b5
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-26 18:50:12 +0900

    Update dataset class

--- gqnlib/scene_dataset.py
+++ gqnlib/scene_dataset.py
@@ -1,13 +1,16 @@
 
 """Dataset class for GQN."""
 
+from typing import Tuple
+
 import gzip
 import pathlib
 
 import torch
+from torch import Tensor
 
 
-class SceneDataset(torch.nn.utils.Dataset):
+class SceneDataset(torch.utils.data.Dataset):
     """SceneDataset class for GQN.
 
     SceneDataset class loads data files at each time accessed by index.
@@ -30,20 +33,55 @@ class SceneDataset(torch.nn.utils.Dataset):
 
         return len(list(self.root_dir.iterdir()))
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> Tuple[Tensor, Tensor]:
         """Load and get data with specified index.
 
         This method read `<index>.pt.gz` file, which includes list of tuples,
-        `(frames, cameras)`.
+        `(images, viewpoints)`.
 
         Args:
-            index (int): Index number
+            index (int): Index number.
+
+        Returns:
+            images (torch.Tensor): Image tensor, size
+                `(batch, seqlen, 3, 64, 64)`.
+            viewpoints (torch.Tensor): View points, size `(batch, seqlen, 7)`.
         """
 
-        with gzip.open(str(self.root_dir), "rb") as f:
-            dataset = f.read()
+        filename = f"{index}.pt.gz"
+        with gzip.open(self.root_dir / filename, "rb") as f:
+            dataset = torch.load(f)
 
-        frames = torch.stack([torch.from_numpy(data[0]) for data in dataset])
-        cameras = torch.stack([torch.from_numpy(data[1]) for data in dataset])
+        images = torch.stack([torch.from_numpy(data[0]) for data in dataset])
+        viewpoints = torch.stack(
+            [torch.from_numpy(data[1]) for data in dataset])
 
-        return frames, cameras
+        # Convert data size: BNHWC -> BNCHW
+        images = images.permute(0, 1, 4, 2, 3)
+
+        # Transform viewpoints
+        viewpoints = transform_viewpoint(viewpoints)
+
+        return images, viewpoints
+
+
+def transform_viewpoint(viewpoints: Tensor) -> Tensor:
+    """Transforms viewpoints.
+
+    (x, y, z, yaw, pitch)
+        -> (x, y, z, cos(yaw), sin(yaw), cos(pitch), sin(pitch))
+
+    Args:
+        viewpoints (torch.Tensor): Input viewpoints, size `(*, 5)`.
+
+    Returns:
+        converted (torch.Tensor): Transformed viewpoints, size `(*, 7)`.
+    """
+
+    pos, tmp = torch.split(viewpoints, 3, dim=-1)
+    yaw, pitch = torch.split(tmp, 1, dim=-1)
+
+    view = [pos, torch.cos(yaw), torch.sin(yaw),
+            torch.cos(pitch), torch.sin(pitch)]
+    view = torch.cat(view, dim=-1)
+    return view
