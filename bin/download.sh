4d053c0c061fc7103eda73740c025636588b9090
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-26 18:19:21 +0900

    Update bin download file

--- bin/download.sh
+++ bin/download.sh
@@ -1,5 +1,6 @@
 
 # Download utility
+# bash bin/downloasd.sh <dataset-name> <batch-size>
 
 # Dataset name mast be one of the following
 # * jaco
@@ -10,21 +11,31 @@
 # * shepard_metzler_5_parts
 # * shepard_metzler_7_parts
 
-export DATA_DIR=./data/
+# Kwargs
 export DATASET_NAME=${1:-shepard_metzler_5_parts}
+export BATCH_SIZE=${2:-32}
+
+# Path
+export DATA_DIR=./data/
+export DATASET_DIR=${DATA_DIR}/${DATASET_NAME}/
 
 # Check data dir
-if [ ! -d ${DATA_DIR} ]; then
+if [[ ! -d ${DATA_DIR} ]]; then
     echo "Make data dir"
     mkdir ${DATA_DIR}
 fi
 
-cd ${DATA_DIR}
-
 # Download dataset
-if [ ! -d ${DATASET_NAME} ]; then
+if [[ ! -d ${DATASET_DIR} ]]; then
     echo "Download dataset"
     gsutil cp -r gs://gqn-dataset/${DATASET_NAME}/ .
 else
     echo "Specified dataset already exists"
 fi
+
+# Convert tfrecords to gzip files
+python3 ./examples/convert_tfrecord_torch.py --dataset ${DATASET_NAME} \
+    --mode train --batch-size ${BATCH_SIZE}
+
+python3 ./examples/convert_tfrecord_torch.py --dataset ${DATASET_NAME} \
+    --mode test --batch-size ${BATCH_SIZE}
