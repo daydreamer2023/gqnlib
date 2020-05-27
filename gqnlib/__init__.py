4d2fd7d404531839cdabacd2369220822ee84c5a
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-28 02:48:14 +0900

    Add scheduler

--- gqnlib/__init__.py
+++ gqnlib/__init__.py
@@ -3,3 +3,4 @@ from .generation import ConvolutionalDRAW
 from .gqn import GenerativeQueryNetwork
 from .representation import Pyramid, Tower
 from .scene_dataset import SceneDataset, partition
+from .scheduler import AnnealingStepLR, Annealer
