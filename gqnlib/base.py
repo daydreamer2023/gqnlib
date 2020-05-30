81c56175b1a71828627e22be02651f55fd6b14f4
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-05-30 10:36:58 +0900

    Update docstrings in base class

--- gqnlib/base.py
+++ gqnlib/base.py
@@ -29,7 +29,7 @@ class BaseGQN(nn.Module):
 
     def loss_func(self, x_c: Tensor, v_c: Tensor, x_q: Tensor, v_q: Tensor,
                   var: float = 1.0) -> Dict[str, Tensor]:
-        """ELBO loss with separated nll and kl loss in dict.
+        """Returns ELBO loss with separated nll and kl losses in dict.
 
         Args:
             x_c (torch.Tensor): Context images, size `(b, m, c, h, w)`.
@@ -47,7 +47,7 @@ class BaseGQN(nn.Module):
 
     def reconstruct(self, x_c: Tensor, v_c: Tensor, x_q: Tensor, v_q: Tensor
                     ) -> Tuple[Tensor, ...]:
-        """Reconstruct given query images.
+        """Reconstructs given query images with contexts.
 
         Args:
             x_c (torch.Tensor): Context images, size `(b, m, c, h, w)`.
@@ -56,7 +56,7 @@ class BaseGQN(nn.Module):
             v_q (torch.Tensor): Query viewpoints, size `(b, n, k)`.
 
         Returns:
-            losses (tuple of torch.Tensor): Tuple of losses.
+            data (tuple of torch.Tensor): Tuple of images and representations.
         """
 
         data, _ = self.inference(x_c, v_c, x_q, v_q)
@@ -65,7 +65,9 @@ class BaseGQN(nn.Module):
     def inference(self, x_c: Tensor, v_c: Tensor, x_q: Tensor, v_q: Tensor,
                   var: float = 1.0
                   ) -> Tuple[Tuple[Tensor, ...], Dict[str, Tensor]]:
-        """Inference.
+        """Inferences with context and target data to calculate ELBO loss.
+
+        **Caution**: Returned `loss_dict` must include `loss` key.
 
         Args:
             x_c (torch.Tensor): Context images, size `(b, m, c, h, w)`.
@@ -75,8 +77,8 @@ class BaseGQN(nn.Module):
             var (float, optional): Variance of observations normal dist.
 
         Returns:
-            data (torch.Tensor): Tuple of inferenced data. Size of each tensor
-                is `(b, n, c, h, w)`.
+            data (tuple of torch.Tensor): Tuple of inferenced data. Size of
+                each tensor is `(b, n, c, h, w)`.
             loss_dict (dict of [str, torch.Tensor]): Calculated losses.
         """
 
