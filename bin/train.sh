8f257527b14e40e45c595e9d74d00c41f773dbbc
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-06-02 17:34:12 +0900

    Update training scripts

--- bin/train.sh
+++ bin/train.sh
@@ -3,14 +3,15 @@
 
 # Settings
 MODEL=gqn
-DATASET=shepard_metzler_5_parts
 CUDA=0,1
+SEED=0
 STEPS=2000000
 TEST_INTERVAL=200000
+DATASET=shepard_metzler_5_parts
 
 # Log path
 export LOGDIR=./logs/
-export EXPERIMENT_NAME=${DATASET}
+export EXPERIMENT_NAME=${MODEL}
 
 # Dataset path
 export DATASET_DIR=./data/
@@ -19,5 +20,5 @@ export DATASET_NAME=${DATASET}_torch
 # Config for training
 export CONFIG_PATH=./examples/config.json
 
-python3 ./examples/train.py --cuda ${CUDA} --model ${MODEL} --steps ${STEPS} \
-    --test-interval ${TEST_INTERVAL}
+python3 ./examples/train.py --cuda ${CUDA} --model ${MODEL} --seed ${SEED} \
+    --steps ${STEPS} --test-interval ${TEST_INTERVAL}
